058935906defb8acb57ed9933cabf9f5d6afe076
nameuser4gane <hacz115@gmail.com>
2024-04-16 21:34:34 -0500

    added a resizer and load option
    resize torch to get smaller and eventually  lead to new game over scene.

---
+++ FinalGame/Assets/Scripts/Resizer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Resizer : MonoBehaviour
+{
+    public float scale = 1.00f;
+
+
+
+    void Update()
+    {
+        scale -= 0.001f;
+        transform.localScale = new Vector2(scale, scale);
+        if (scale <= 0.05f)
+            SceneManager.LoadScene("");
+    }
+}
