d254d3b8fa0b8db863f78999474555505ae9f8e6
stefch07 <schar9360@365.elmhurst.edu>
2024-04-18 00:47:58 -0500

    game over scene added

---
+++ FinalGame/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameOver GameOverScreen;
+    int maxPlatform = 0;
+
+    public void GameOver()
+    {
+        GameOverScreen.Setup(maxPlatform);
+    }
+
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    
+
+}
---
+++ FinalGame/Assets/Scripts/GameOver.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public Text pointsText; // Define the pointsText variable of type Text
+
+    public void Setup(int score)
+    {
+        gameObject.SetActive(true);
+        pointsText.text = score.ToString() + " POINTS";
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
