8ef2a7664de3e400c78f34e20d76c1d5efa1c138
TheGreenStarCollector <tptas5337@365.elmhurst.edu>
2024-04-18 22:31:27 -0500

    Added ScoreManager script.

--- FinalGame/Assets/Scripts/NewTriggerZone.cs
+++ FinalGame/Assets/Scripts/NewTriggerZone.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+
 public class NewTriggerZone : MonoBehaviour
 {
     // create a variable to keep track of
---
+++ FinalGame/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    
+    // notice public static variables
+    // can be accessed from any script
+    // but cannot be seen in the inspector
+    public static bool gameOver;
+    public static bool won;
+    public static int score;
+    
+    public TMP_Text textbox;
+    
+    // public int ScoreToWin;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        won = false;
+        score = 0;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            textbox.text = "Souls: " + score;
+        }
+        
+        if (score >= 3)
+        {
+            won = true;
+            gameOver = true;
+        }
+        
+        if (gameOver)
+        {
+            if (won)
+            {
+            }
+            else
+            {
+            }
+        }
+    }
+}
