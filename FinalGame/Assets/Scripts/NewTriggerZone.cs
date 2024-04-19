f774ec05b959039fc5fb7d15d37a0072229677e8
stefch07 <schar9360@365.elmhurst.edu>
2024-04-19 13:32:25 -0500

    .

--- FinalGame/Assets/Scripts/NewTriggerZone.cs
+++ FinalGame/Assets/Scripts/NewTriggerZone.cs
@@ -23,4 +23,8 @@ public class NewTriggerZone : MonoBehaviour
             gameObject.SetActive(false);
         }
     }
+
+
+
+
 }
--- FinalGame/Assets/Scripts/SoundZone.cs
+++ FinalGame/Assets/Scripts/SoundZone.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class SoundZone : MonoBehaviour
 {
+    // Add an AudioClip variable to hold the sound effect
+    public AudioClip soulSound;
+
     // Create a variable to keep track of whether the trigger zone is active
-    private AudioSource soulSound;
-    bool active = true;
-    
-    private void Start()
-    {
-        soulSound = GetComponent<AudioSource>();
-    }
-    
+    private bool active = true;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // if the trigger zone is active...
         if (active && collision.gameObject.tag == "Player")
         {
-            soulSound.Play();
-            
-            // deactivate the trigger zone
+            // Play the sound effect
+            AudioSource.PlayClipAtPoint(soulSound, transform.position);
+
+            // Deactivate the trigger zone
             active = false;
-            
+
+            // Deactivate the GameObject
             gameObject.SetActive(false);
         }
     }
