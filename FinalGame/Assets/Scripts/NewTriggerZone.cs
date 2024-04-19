df6f67809efaea3bdb115bb23381ffbcdbb202a8
stefch07 <schar9360@365.elmhurst.edu>
2024-04-18 23:13:41 -0500

    .

--- FinalGame/Assets/Scripts/NewTriggerZone.cs
+++ FinalGame/Assets/Scripts/NewTriggerZone.cs
@@ -6,18 +6,18 @@ public class NewTriggerZone : MonoBehaviour
 {
     // create a variable to keep track of
     // whether the trigger zone is active
-    
+
     bool active = true;
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+
         // if the trigger zone is active...
         if (active && collision.gameObject.tag == "Player")
         {
             // deactivate the trigger zone
             active = false;
-            
+
             // Adds 1 to the core
             // when the player enters the trigger zone
             ScoreManager.score++;
