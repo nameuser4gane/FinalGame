12a1e67270c680ffb018f255b047718ec744e7e7
TheGreenStarCollector <tptas5337@365.elmhurst.edu>
2024-04-19 22:25:52 -0500

    Souls now play a sound and disappear when collected; deleted unnecessary SoundZone script.

--- FinalGame/Assets/Scripts/NewTriggerZone.cs
+++ FinalGame/Assets/Scripts/NewTriggerZone.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 
 public class NewTriggerZone : MonoBehaviour
 {
-    // create a variable to keep track of
-    // whether the trigger zone is active
-
+    // Create a variable to keep track of whether the trigger zone is active
     bool active = true;
-
+    
+    public AudioClip soulSound;
+    private AudioSource zoneAudio;
+    
+    private void Start()
+    {
+        // Set reference variables to components
+        zoneAudio = GetComponent<AudioSource>();
+    }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // if the trigger zone is active...
         if (active && collision.gameObject.tag == "Player")
         {
-            // deactivate the trigger zone
+            // Play collect sound when the player enters the trigger zone
+            zoneAudio.PlayOneShot(soulSound, 1.0f);
+            
+            // Deactivates/hides trigger zone so player can only use once
+            // SetActive(false) causes the object to leave the scene faster than the sound can play,
+            // so for the time being a quick solution is to just make it invisible.
             active = false;
-
-            // Adds 1 to the score
-            // when the player enters the trigger zone
+            //this.gameObject.SetActive(false);
+            this.gameObject.GetComponent<Renderer>().material.color = new Color(250, 174, 90, 0f);
+            
+            // Destroys once the sound has played
+            Destroy(gameObject, 3);
+            
+            // Add 1 to the score when the player enters the trigger zone
             ScoreManager.score++;
-            gameObject.SetActive(false);
         }
     }
-
-
-
-
 }
