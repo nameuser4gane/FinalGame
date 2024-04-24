5f081c35f0c01acb682b9f5a9a68b763c5ffba35
TheGreenStarCollector <tptas5337@365.elmhurst.edu>
2024-04-24 16:56:00 -0500

    Added ScoreManagerDX script to souls in Game scene.

---
+++ FinalGame/Assets/Scripts/ScoreManagerDX.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ScoreManagerDX : MonoBehaviour
+{
+    
+    // notice public static variables
+    // can be accessed from any script
+    // but cannot be seen in the inspector
+    public static bool gameOver;
+    public static bool won;
+    public static int score;
+    
+    public TMP_Text textbox;
+    
+    // public int ScoreToWin;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        won = false;
+        score = 0;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver && score < 7)
+        {
+            textbox.text = "Souls: " + score + "/7";
+        }
+        
+        if (!gameOver && score == 7)
+        {
+            textbox.text = "All souls freed!";
+        }
+        
+        if (score >= 7)
+        {
+            won = true;
+            gameOver = true;
+        }
+        
+        if (gameOver)
+        {
+            if (won)
+            {
+            }
+            else
+            {
+            }
+        }
+    }
+}
