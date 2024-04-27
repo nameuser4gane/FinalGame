83ea29fa2ac8e24a769f36c07d47a3cfd8ff3cd4
TheGreenStarCollector <tptas5337@365.elmhurst.edu>
2024-04-26 20:53:57 -0500

    Trying to fix score manager for Game scene.

--- FinalGame/Assets/Scripts/ScoreManagerDX.cs
+++ FinalGame/Assets/Scripts/ScoreManagerDX.cs
@@ -10,44 +10,44 @@ public class ScoreManagerDX : MonoBehaviour
     // notice public static variables
     // can be accessed from any script
     // but cannot be seen in the inspector
-    public static bool gameOver;
-    public static bool won;
-    public static int score;
+    public static bool gameOverDX;
+    public static bool wonDX;
+    public static int scoreDX;
     
-    public TMP_Text textbox;
+    public TMP_Text textboxDX;
     
     // public int ScoreToWin;
     
     // Start is called before the first frame update
     void Start()
     {
-        gameOver = false;
-        won = false;
-        score = 0;
+        gameOverDX = false;
+        wonDX = false;
+        scoreDX = 0;
     }
     
     // Update is called once per frame
     void Update()
     {
-        if (!gameOver && score < 7)
+        if (!gameOverDX && scoreDX < 7)
         {
-            textbox.text = "Souls: " + score + "/7";
+            textboxDX.text = "Souls: " + scoreDX + "/7";
         }
         
-        if (!gameOver && score == 7)
+        if (!gameOverDX && scoreDX == 7)
         {
-            textbox.text = "All souls freed!";
+            textboxDX.text = "All souls freed!";
         }
         
-        if (score >= 7)
+        if (scoreDX >= 7)
         {
-            won = true;
-            gameOver = true;
+            wonDX = true;
+            gameOverDX = true;
         }
         
-        if (gameOver)
+        if (gameOverDX)
         {
-            if (won)
+            if (wonDX)
             {
             }
             else
