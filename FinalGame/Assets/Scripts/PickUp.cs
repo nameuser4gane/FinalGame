70f0ff9b8b1db844c11cf40169095bda85ad8226
TheGreenStarCollector <tptas5337@365.elmhurst.edu>
2024-04-30 21:25:54 -0500

    Disabled AudioSource for lanterns.

--- FinalGame/Assets/Scripts/PickUp.cs
+++ FinalGame/Assets/Scripts/PickUp.cs
@@ -7,34 +7,29 @@ public class PickUp : MonoBehaviour
     bool active = true;
     // Start is called before the first frame update
     public float time = 10;
-
-    public AudioClip triggerSound;
-
+    
+    //public AudioClip triggerSound;
+    
     public float delay = 2f;
-
+    
     public LightControl remainTime;
-
-    private AudioSource playerAudio;
-
-
+    
+    //private AudioSource playerAudio;
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-      
-
-        playerAudio = GetComponent<AudioSource>();
+        //playerAudio = GetComponent<AudioSource>();
         if (active && collision.gameObject.tag == "Player")
         {
             LightControl lightControl = collision.gameObject.GetComponent<LightControl>();
             
-
             active = false;
-
-            playerAudio.PlayOneShot(triggerSound, 1.0f);
-
+            
+            //playerAudio.PlayOneShot(triggerSound, 1.0f);
+            
             remainTime.AddTime(time);
-
+            
             Destroy(gameObject, delay);
         }
     }
-
 }
